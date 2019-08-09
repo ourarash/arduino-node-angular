4fda3d91709a78d69a3a31c0556d24941b626b37
Ari <ourarash@gmail.com>
2019-08-08 22:06:51 -0700

    First commit

---
+++ angular-websockets-client/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+let i = [];
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-websockets-client';
+  i = [];
+  constructor() {
+    // this.i = [];
+    console.log("We are in constructor");
+    console.log('this.i: ', JSON.stringify(this.i));
+    this.setup();
+  }
+
+  socket = new WebSocket("ws://localhost:8081");
+
+  setup() {
+    // The socket connection needs two event listeners:
+    this.socket.onopen = this.openSocket;
+    this.socket.onmessage = this.showData;
+
+    // make a new div and position it at 10, 10:
+    // this.text = createDiv("Sensor reading:");
+    // this.text.position(10, 10);
+  }
+
+  openSocket() {
+    // this.text.html("Socket open");
+    // this.socket.send("Hello server");
+  }
+
+  showData(result: any) {
+    let str2 = result.data.replace(/(\\r\\n|\\n|\\r)/gm, "");
+    console.log('str2: ', JSON.stringify(str2));
+    if (i) {
+      i.push(str2);
+    }
+
+    console.log('this.i: ', JSON.stringify(i));
+
+    if (i.length > 20) {
+      i.shift();
+    }
+    // console.log('i: ', JSON.stringify(this.i));
+    // when the server returns, show the result in the div:
+    // text.html("Sensor reading:" + i);
+    // xPos = int(result.data); // convert result to an integer
+    // text.position(xPos, 10); // position the text
+  }
+
+  getArray() {
+    return i;
+  }
+
+  sendGame(n: number) {
+    // ledNumber, pushButtonNumber, Duration, Blocking
+    // let game1 = 
+    // [[1, 1, 100, false],
+    // [2, 1, 100, false],
+    // [3, 1, 100, false]];
+
+    let game1 =
+      [1, 1, 100, 0];
+
+    // [[1,2], [3,4], [5, 6]]
+    console.log('game1: ', JSON.stringify(game1));
+    this.socket.send(game1.join(','));
+    // for(let e of game1){
+    //   this.socket.send(e.toString());
+
+    // }
+  }
+}
---
+++ node-server/index.js
@@ -0,0 +1,87 @@
+/*
+Serial-to-websocket Server
+using serialport.js
+
+To call this type the following on the command line:
+node wsServer.js portName
+
+where portname is the name of your serial port, e.g. /dev/tty.usbserial-xxxx (on OSX)
+
+created 28 Aug 2015
+modified 5 Nov 2017
+by Tom Igoe
+
+*/
+
+// include the various libraries that you'll use:
+var SerialPort = require('serialport');			// include the serialport library
+var	portName =  '/dev/cu.usbmodem144301';						// get the port name from the command line
+var WebSocketServer = require('ws').Server;   // include the webSocket library
+
+// configure the webSocket server:
+var SERVER_PORT = 8081;                 // port number for the webSocket server
+var wss = new WebSocketServer({port: SERVER_PORT}); // the webSocket server
+var connections = new Array;            // list of connections to the server
+
+var myPort = new SerialPort(portName, 9600);// open the port
+var Readline = SerialPort.parsers.Readline;	// make instance of Readline parser
+var parser = new Readline();								// make a new parser to read ASCII lines
+myPort.pipe(parser);													// pipe the serial stream to the parser
+
+// these are the definitions for the serial events:
+myPort.on('open', showPortOpen);    // called when the serial port opens
+myPort.on('close', showPortClose);  // called when the serial port closes
+myPort.on('error', showError);   // called when there's an error with the serial port
+parser.on('data', readSerialData);  // called when there's new data incoming
+
+
+// ------------------------ Serial event functions:
+// this is called when the serial port is opened:
+function showPortOpen() {
+  console.log('port open. Data rate: ' + myPort.baudRate);
+}
+
+// this is called when new data comes into the serial port:
+function readSerialData(data) {
+  // if there are webSocket connections, send the serial data
+  // to all of them:
+  console.log(data);
+  if (connections.length > 0) {
+    broadcast(data);
+  }
+}
+
+function showPortClose() {
+   console.log('port closed.');
+}
+// this is called when the serial port has an error:
+function showError(error) {
+  console.log('Serial port error: ' + error);
+}
+
+function sendToSerial(data) {
+  console.log("sending to serial: " + data);
+  myPort.write(data);
+}
+
+// ------------------------ webSocket Server event functions
+wss.on('connection', handleConnection);
+
+function handleConnection(client) {
+  console.log("New Connection");        // you have a new client
+  connections.push(client);             // add this client to the connections array
+
+  client.on('message', sendToSerial);      // when a client sends a message,
+
+  client.on('close', function() {           // when a client closes its connection
+    console.log("connection closed");       // print it out
+    var position = connections.indexOf(client); // get the client's position in the array
+    connections.splice(position, 1);        // and delete it from the array
+  });
+}
+// This function broadcasts messages to all webSocket clients
+function broadcast(data) {
+  for (c in connections) {     // iterate over the array of connections
+    connections[c].send(JSON.stringify(data)); // send the data to each connection
+  }
+}
